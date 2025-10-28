256c9c4bd0fee3329953292a16b6428f0fc10ba5
vanshfried <vanshfried@gmail.com>
2025-10-28 16:47:59 +0530

    good guy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { AuthProvider } from "../src/pages/admin/Context/AuthContext.jsx"; // adjust path
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
